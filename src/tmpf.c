47b3482b2fc19a9929413eb6895c2884987d2e6d
Minqi Pan <pmq2001@gmail.com>
2017-07-03 12:09:00 +0800

    add util c files

---
+++ src/tmpf.c
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2017 Minqi Pan <pmq2001@gmail.com>
+ *
+ * This file is part of libautoupdate, distributed under the MIT License
+ * For full terms see the included LICENSE file
+ */
+
